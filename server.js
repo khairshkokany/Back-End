d9ffe4c99deee470a0b3c034380d7e58efc898f0
khair eldeen zakaria shkokany <shkokany98@gmail.com>
2021-08-03 01:32:06 +0300

    final editing

--- server.js
+++ server.js
@@ -1,38 +1,68 @@
 'use strict';
 
 const express = require('express');
-
+const cors = require ('cors');
 const weather = require('./data/weather.json');
-
-// const PORT = process.env.PORT;
-const PORT = 3000;
-
 const server = express();
+server.use(cors());
+require('dotenv').config();
+
+
+const PORT = process.env.PORT;
+// const PORT = 3000;
+
 
 
 // http://localhost:3000/weather
 
 server.get('/weather',(req,res)=>{
 
-  let weatherData = weather.map(item =>{
-    console.log(weather);
-    // const lon = req.query.lon;
-    // const lat = req.query.lat;
-    const searchQuery = req.query.city_name;
-    // if (item.searchQuery === item.city_name) {
-      console.log(searchQuery);
-        // return `${item.lon} , ${item.lat} , ${item.city_name} , ${item.data[2].weather.description}`;
-      return `${item.lon} , ${item.lat} , ${item.city_name} , ${item.data[2].weather.description} ${item.data[0].valid_date}`;
-    // }else {
-    //   return 'Error 404 Not Found';
-    // }
+  let weatherData = weather.find(item =>{
+    // console.log(weather);
+    
+    const lon = req.query.lon;
+    const lat = req.query.lat;
+    const searchQuery = req.query.searchQuery;
+    if (item.city_name == searchQuery || item.lon == lon || item.lat == lat) {
+      console.log(item.city_name);
+      // return `${item.lon} , ${item.lat} , ${item.city_name} , ${item.data[2].weather.description}`;
+      return item.city_name;
+    //   return `${item.lon} , ${item.lat} , ${item.city_name} , ${item.data[2].weather.description} ${item.data[0].datetime,item.data[1].datetime,item.data[2].datetime}`;
+    }else {
+      return 'Error 404 Not Found';
+    }
   });
 
-  res.send(weatherData);
+
+
+
+  class Forecast {
+    constructor(datetime,description) {
+
+      this.date = datetime;
+      this.description = description;
+    }
+  }
+  let newArr = [];
+  weatherData.data.map((item) => {
+
+
+    newArr.push(new Forecast(item.datetime, `Low of ${item.low_temp} , high of ${item.high_temp} , with ${item.weather.description}`));
+  });
+
+  res.send(newArr);
+    //   res.send(weatherData);
+
+
 });
 
 server.listen(PORT,()=>{
 
   console.log(`hello from server this is your port ${PORT}`);
-//   console.log(weather);
+  //   console.log(weather);
 });
+
+server.get('*', (req,res) => {
+  res.status(404).send('page not found');
+});
+
